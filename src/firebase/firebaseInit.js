44f58434447a54df47cefadc3bd5bd1f9a67db7d
marcus <https://github.com/marcuspure>
2021-07-01 19:12:36 +0800

    創

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCj50M_NzuxSEIC748ylQyfjqAzRc9DaNA",
+  authDomain: "vueproject-4d7e4.firebaseapp.com",
+  projectId: "vueproject-4d7e4",
+  storageBucket: "vueproject-4d7e4.appspot.com",
+  messagingSenderId: "809736953091",
+  appId: "1:809736953091:web:179bec92e0e93ec0a51d36",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const timestamp = firebase.firestore.FieldValue.serverTimerstamp;
+
+export { timestamp };
+export default firebaseApp.firestore();
